620e1d6887dd3bafcc234954fb6bc9b583b82a19
magnusanatolius <aytac@linux.com>
2020-02-11 21:44:23 +0100

    dockerfile is added.

--- keycloak/build.sh
+++ keycloak/build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker build -t in-sylva.keycloak .
+docker build -t in-sylva.keycloak --build-arg SSH_KEY="$(cat ~/.ssh/id_ed25519)" --build-arg SSH_KEY_PASSPHRASE="$(cat ~/.ssh/id_ed25519.pub)" --build-arg KNOWN_HOSTS="$(cat ~/.ssh/known_hosts)" --squash .
 
