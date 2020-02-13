c0a3bd718c263cbafbb86c2d582545d2367ba87b
magnusanatolius <aytac@linux.com>
2020-02-13 21:45:59 +0100

    portal dockerfile is updated with nginx.conf

---
+++ portal/build.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+SSH_KEY=$(cat ~/.ssh/id_rsa_insylva_docker)
+SSH_KEY_PASSPHRASE="$(cat ~/.ssh/id_rsa_insylva_docker.pub)"
+
+docker build --build-arg SSH_KEY="$SSH_KEY" --build-arg SSH_KEY_PASSPHRASE="$SSH_KEY_PASSPHRASE"  --tag in-sylva.portal .
