ff3b79bf497786e3025e323f3496877bb23a303c
magnusanatolius <aytac@linux.com>
2020-02-13 17:41:22 +0100

    ssh key support is added to gatekeeper.

--- gatekeeper/build.sh
+++ gatekeeper/build.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-docker build -t in-sylva.gatekeeper .
+SSH_KEY=$(cat ~/.ssh/id_rsa_insylva_docker)
+# SSH_KEY=$(cat ./id_rsa)
+SSH_KEY_PASSPHRASE="$(cat ~/.ssh/id_rsa_insylva_docker.pub)"
+# --build-arg SSH_KEY_PASSPHRASE="$SSH_KEY_PASSPHRASE"
+docker build --build-arg SSH_KEY="$SSH_KEY" --build-arg SSH_KEY_PASSPHRASE="$SSH_KEY_PASSPHRASE" --tag in_sylva_gatekeeper .
+# docker build -t in_sylva_keycloak --build-arg SSH_KEY="$(cat ~/.ssh/id_rsa_insylva_docker)" --build-arg SSH_KEY_PASSPHRASE="$(cat ~/.ssh/id_rsa_insylva_docker.pub)" --build-arg KNOWN_HOSTS="$(cat ~/.ssh/known_hosts)" --squash --net=host . 
+
--- keycloak/build.sh
+++ keycloak/build.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
-SSH_KEY=$(cat ~/.ssh/id_rsa_insylva_docker)
-SSH_KEY_PASSPHRASE="$(cat ~/.ssh/id_rsa_insylva_docker.pub)"
-docker build --build-arg SSH_KEY="$SSH_KEY" --build-arg SSH_KEY_PASSPHRASE="$SSH_KEY_PASSPHRASE" --tag in_sylva_keycloak --squash --net=host .
-# docker build -t in_sylva_keycloak --build-arg SSH_KEY="$(cat ~/.ssh/id_rsa_insylva_docker)" --build-arg SSH_KEY_PASSPHRASE="$(cat ~/.ssh/id_rsa_insylva_docker.pub)" --build-arg KNOWN_HOSTS="$(cat ~/.ssh/known_hosts)" --squash --net=host . 
+docker build --tag in_sylva_keycloak .
 
