7338ba102fdfae752a5ff14a0a191e0bb6f1f5c5
magnusanatolius <aytac@linux.com>
2020-02-13 16:21:14 +0100

    ssh settings for gatekeeper

--- keycloak/build.sh
+++ keycloak/build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-docker build -t in-sylva.keycloak --build-arg SSH_KEY="$(cat ~/.ssh/id_ed25519)" --build-arg SSH_KEY_PASSPHRASE="$(cat ~/.ssh/id_ed25519.pub)" --build-arg KNOWN_HOSTS="$(cat ~/.ssh/known_hosts)" --squash .
+SSH_KEY=$(cat ~/.ssh/id_rsa_insylva_docker)
+SSH_KEY_PASSPHRASE="$(cat ~/.ssh/id_rsa_insylva_docker.pub)"
+docker build --build-arg SSH_KEY="$SSH_KEY" --build-arg SSH_KEY_PASSPHRASE="$SSH_KEY_PASSPHRASE" --tag in_sylva_keycloak --squash --net=host .
+# docker build -t in_sylva_keycloak --build-arg SSH_KEY="$(cat ~/.ssh/id_rsa_insylva_docker)" --build-arg SSH_KEY_PASSPHRASE="$(cat ~/.ssh/id_rsa_insylva_docker.pub)" --build-arg KNOWN_HOSTS="$(cat ~/.ssh/known_hosts)" --squash --net=host . 
 
