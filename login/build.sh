e931a880e2cefc8f03132ada125bd2559565d5aa
magnusanatolius <aytac@linux.com>
2020-02-14 11:54:53 +0100

    general improvement.

---
+++ login/build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+ while [ "$1" != "" ]; do
+   SSH_KEY=$(cat ~/.ssh/$1)
+   SSH_KEY_PASSPHRASE=$(cat ~/.ssh/$1.pub)
+    # Shift all the parameters down by one
+    shift
+done
+
+docker build --build-arg SSH_KEY="$SSH_KEY" --build-arg SSH_KEY_PASSPHRASE="$SSH_KEY_PASSPHRASE" --tag in-sylva.user.login .
+
+
---
+++ login/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run -d -p 8081:8080 in-sylva.user.login:latest
