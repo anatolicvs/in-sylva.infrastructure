e25d5e5806b4374dbbcf3528b2d324daf318e9a4
magnusanatolius <aytac@linux.com>
2020-02-17 18:53:55 +0100

    docker-compose file is updated.

--- source.manager/build.sh
+++ source.manager/build.sh
@@ -6,7 +6,7 @@ while [ "$1" != "" ]; do
    SSH_KEY=$(cat ~/.ssh/$1)
    SSH_KEY_PASSPHRASE=$(cat ~/.ssh/$1.pub)
     # Shift all the parameters down by one
-    shift
+   shift
 done
 
 docker build --build-arg SSH_KEY="$SSH_KEY" --build-arg SSH_KEY_PASSPHRASE="$SSH_KEY_PASSPHRASE" --tag in-sylva.source.manager .
