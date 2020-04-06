829daa2647daab0dce9ee2bfe6737c33c9d59ae8
magnus anatolius <magnusanatolius@localhost.localdomain>
2020-04-06 17:01:41 +0200

    *

--- source.manager/build.sh
+++ source.manager/build.sh
@@ -7,4 +7,4 @@ while [ "$1" != "" ]; do
     shift
 done
 
-docker build --no-cache  --build-arg SSH_KEY="$SSH_KEY" --build-arg SSH_KEY_PASSPHRASE="$SSH_KEY_PASSPHRASE" --tag in-sylva.source.manager .
+docker build --no-cache  --build-arg SSH_KEY="$SSH_KEY" --build-arg SSH_KEY_PASSPHRASE="$SSH_KEY_PASSPHRASE" --tag in-sylva.source.manager ./source.manager/.
