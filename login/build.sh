d41f7f2bc46301c4c1ea5550869a1f73ba1f6a55
Aytac Ozkan <aytac.ozkan@inra.fr>
2020-05-25 10:37:32 +0200

    search-app settings updated.

--- login/build.sh
+++ login/build.sh
@@ -7,15 +7,16 @@ while [ "$1" != "" ]; do
     shift
 done
 
-docker build --no-cache --network=host --no-cache --build-arg SSH_KEY="$SSH_KEY" --build-arg SSH_KEY_PASSPHRASE="$SSH_KEY_PASSPHRASE" \
- --build-arg IN_SYLVA_GATEKEEPER_HOST="0.0.0.0" \
- --build-arg IN_SYLVA_GATEKEEPER_PORT="4000" \
- --build-arg IN_SYLVA_KEYCLOAK_HOST="0.0.0.0" \
- --build-arg IN_SYLVA_KEYCLOAK_PORT="3000" \
- --build-arg IN_SYLVA_SEARCH_HOST="0.0.0.0" \
- --build-arg IN_SYLVA_SEARCH_PORT="3001" \
- --build-arg IN_SYLVA_PORTAL_HOST="0.0.0.0" \
- --build-arg IN_SYLVA_PORTAL_PORT="3000" \
+# --build-arg IN_SYLVA_KEYCLOAK_HOST="0.0.0.0" \
+# --build-arg IN_SYLVA_KEYCLOAK_PORT="7000" \
+# --build-arg IN_SYLVA_PORTAL_HOST="0.0.0.0" \
+# --build-arg IN_SYLVA_PORTAL_PORT="3000" \
+# --build-arg IN_SYLVA_SEARCH_HOST="0.0.0.0" \
+# --build-arg IN_SYLVA_SEARCH_PORT="3001" \
+# --build-arg IN_SYLVA_GATEKEEPER_HOST="0.0.0.0" \
+# --build-arg IN_SYLVA_GATEKEEPER_PORT="4000" \
+
+docker build --no-cache --build-arg SSH_KEY="$SSH_KEY" --build-arg SSH_KEY_PASSPHRASE="$SSH_KEY_PASSPHRASE" \
  --build-arg IN_SYLVA_CLIENT_ID="in-sylva.user.app" \
  --build-arg IN_SYLVA_GRANT_TYPE="password" \
  --build-arg IN_SYLVA_REALM="in-sylva" \
